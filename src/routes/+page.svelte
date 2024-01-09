dfe16823dee779fb72afd0cb0ef84eb0bd77381d
Pedro Leonardo <pedroleon.pc@hotmail.com>
2024-01-09 11:35:31 -0300

    Revert "Mudança na implicitação"
    This reverts commit 81007b400ca35694c6584cbb4bbd1f8d53ff4c7e.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,2 @@
-<script>
-    import Home from "./home/home.svelte";
-</script>
-<main>
-    <Home/>
-</main>
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-
-    *{
-        font-family: 'Montserrat', Arial;
-        color: white;
-    }
-    main{
-    background-color: #0D1117;
-    width: 100%;
-    overflow-x: hidden;
-    }
-</style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
