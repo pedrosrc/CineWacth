c6e73c5712e383ea58f585644936bfe394891de5
Pedro Leonardo <pedroleon.pc@hotmail.com>
2024-01-12 12:09:03 -0300

    <style> Mudança no estilo do header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,6 @@
     <Home/>
 </section>
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-    *{
-        font-family: 'Montserrat', Arial;
-        color: white;
-    }
     section{
     background-color: #0D1117;
     width: 100%;
