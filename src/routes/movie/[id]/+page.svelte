6684283a354dae832ba65160326a6722801d4300
Pedro Leonardo <pedroleon.pc@hotmail.com>
2024-01-12 12:14:00 -0300

    aplicação de bibliotecas de icones e rating

--- src/routes/movie/[id]/+page.svelte
+++ src/routes/movie/[id]/+page.svelte
@@ -2,6 +2,9 @@
     import { page } from "$app/stores";
     import { onMount } from "svelte";
     import api from "../../../services/api";
+    import StarRating from "@ernane/svelte-star-rating";
+    import IoIosHeartEmpty from 'svelte-icons/io/IoIosHeartEmpty.svelte'
+    import MdPlayCircleOutline from 'svelte-icons/md/MdPlayCircleOutline.svelte'
 
     let movieId = $page.params;
     let movie;
@@ -17,27 +20,27 @@
     });
 </script>
 
-<section>
+<section class="container_movie">
     {#if movie}
-        <div class="container_background"></div>
         <div class="container_info">
             <div class="poster">
-                <img src="" alt="" />
+                <img
+                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                    alt=""
+                />
             </div>
-            <div class="section_">
+            <div class="section_info">
                 <h1>{movie.title}</h1>
-                <span>Nota</span>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Beatae veniam qui laboriosam totam dolore inventore tempore
-                    minus magnam, exercitationem ab perspiciatis provident earum
-                    voluptas, eius mollitia facilis illum sed est?
-                </p>
-                <div>
-                    <button>Salvar</button>
-                    <button>Trailer</button>
+                <span>Nota: {movie.vote_average.toFixed(1)}</span>
+                <p>{movie.overview}</p>
+                <div class="buttons">
+                    <button><svg><IoIosHeartEmpty/></svg> Salvar</button>
+                    <button><svg><MdPlayCircleOutline/></svg>Trailer</button>
                 </div>
             </div>
+        </div>
+        <div class="container_location">
+            <h2>Aonde assistir?</h2>
         </div>
     {:else}
         <div>Caregando...</div>
@@ -45,9 +48,52 @@
 </section>
 
 <style>
-    .container_background {
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
+    .container_movie {
+        position: relative;
+    }
+    .container_info {
+        display: flex;
+        width: 1000px;
+        margin: auto;
+        margin-top: 2em;
+    }
+    .container_location {
+        display: flex;
+        font-style: italic;
+        flex-direction: column;
+        width: 1000px;
+        margin: auto;
+        margin-top: 2em;
+    }
+    .poster img {
+        width: 300px;
+        border-radius: 15px;
+    }
+    .section_info {
+        margin-left: 10px;
+    }
+    .section_info p{
+        width: 30em;
+        font-size: 1.2em;
+    }
+    .buttons{
+        display: flex;
+    }
+    .buttons button{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        width: 120px;
+        height: 40px;
+        margin-right: 10px;
+        border-radius: 5px;
+        background-color: blueviolet;
+        color: #fff;
+    }
+    .buttons button svg{
+        width: 20px;
+        margin: 0;
+        padding: 0;
     }
 </style>
